43c642b0fa49d3d2e34fd12ccc697242f10ab65f
Azam <gulovazam@gmail.com>
2023-01-10 20:42:49 +0500

    Add home components

---
+++ src/modules/home.js
@@ -0,0 +1,82 @@
+import { navbar } from "./header";
+import fishTacos from "../assets/fish-tacos.jpg";
+import chickenTostadas from "../assets/chicken-tostadas.jpg";
+import bakedChickenQuesadillas from "../assets/Baked-Chicken-Quesadillas.jpg";
+import chickenEnchiladas from "../assets/chicken-enchiladas.jpg";
+
+const featuredMenu = {
+  item1: {
+    name: "Fish Tacos",
+    img: fishTacos,
+  },
+  item2: {
+    name: "Chicken Tostadas",
+    img: chickenTostadas,
+  },
+  item3: {
+    name: "Baked Chicken Quesadillas",
+    img: bakedChickenQuesadillas,
+  },
+  item4: {
+    name: "Chicken Enchiladas",
+    img: chickenEnchiladas,
+  },
+};
+
+export default function home() {
+  const homeSection = document.createElement("section");
+
+  homeSection.classList.add("home-section");
+
+  homeSection.appendChild(navbar());
+  homeSection.appendChild(infoComponent());
+  homeSection.appendChild(featuredItems());
+
+  return homeSection;
+}
+
+function infoComponent() {
+  const infoWrapper = document.createElement("div");
+  const logo = document.createElement("p");
+  const restaurantDisc = document.createElement("h1");
+
+  infoWrapper.classList.add("info-wrapper");
+  logo.classList.add("main-logo");
+  restaurantDisc.classList.add("rest-disc");
+
+  logo.textContent = "Luna";
+  restaurantDisc.textContent = "Delicious Specialties";
+
+  infoWrapper.appendChild(logo);
+  infoWrapper.appendChild(restaurantDisc);
+
+  return infoWrapper;
+}
+
+function featuredItems() {
+  const featuredItemsContainer = document.createElement("div");
+  const featuredItemsWrapper = document.createElement("div");
+
+  featuredItemsContainer.classList.add("featured-items");
+  featuredItemsWrapper.classList.add("featured-items-wrapper");
+
+  Object.values(featuredMenu).forEach((item) => {
+    const featuredItem = document.createElement("div");
+    const itemImg = new Image();
+    const itemName = document.createElement("p");
+
+    featuredItem.classList.add("featured-item");
+
+    itemImg.src = item.img;
+    itemImg.draggable = false;
+    itemName.textContent = item.name;
+
+    featuredItem.appendChild(itemImg);
+    featuredItem.appendChild(itemName);
+    featuredItemsWrapper.appendChild(featuredItem);
+  });
+
+  featuredItemsContainer.appendChild(featuredItemsWrapper);
+
+  return featuredItemsContainer;
+}
