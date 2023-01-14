08ed0c4c6284d902f275594fff1874d335a44359
Gulov Azam <gulovazam@gmail.com>
2023-01-14 22:14:49 +0500

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import "./styles/style.css";
+import { navbar, header } from "./modules/header";
 import home from "./modules/home";
+import menu from "./modules/menu";
 
-document.getElementById("content").appendChild(home());
+const content = document.getElementById("content");
+
+content.addEventListener("click", tabSwitching);
+
+function defaultPage() {
+  content.appendChild(home());
+  document.querySelector(".home-link").classList.add("active");
+}
+
+defaultPage();
+
+function tabSwitching(e) {
+  if (e.target.matches(".nav-item")) {
+    content.textContent = "";
+  }
+
+  if (e.target.matches(".home-link")) {
+    content.append(home());
+    document.querySelector(".home-link").classList.add("active");
+  }
+
+  if (e.target.matches(".menu-link")) {
+    e.target.classList.add("active");
+    content.append(header(), menu());
+    document.querySelector(".menu-link").classList.add("active");
+  }
+}
