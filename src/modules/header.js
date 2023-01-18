c37387d340c2a81db10335de50ce6e966be390a2
Azam <gulovazam@gmail.com>
2023-01-18 20:49:37 +0500

    Add nav collapse components

--- src/modules/header.js
+++ src/modules/header.js
@@ -5,21 +5,31 @@ function navbar() {
   const navWrapper = document.createElement("div");
   const logo = document.createElement("p");
   const navLinks = document.createElement("ul");
+  const navLinksCopy = navLinks.cloneNode(true);
+  const navToggler = document.createElement("button");
+  const navCollapse = document.createElement("div");
 
   navWrapper.classList.add("nav-wrapper");
   navLinks.classList.add("main-nav");
   logo.classList.add("logo-header");
+  navToggler.classList.add("nav-toggler");
+  navCollapse.classList.add("nav-collapse");
   logo.textContent = "Luna";
+  navToggler.textContent = "menu";
 
   navElement.appendChild(navWrapper);
   navWrapper.appendChild(logo);
   navWrapper.appendChild(navLinks);
+  navWrapper.appendChild(navToggler);
+  navWrapper.appendChild(navCollapse);
+  navCollapse.appendChild(navLinksCopy);
 
   links.forEach((link) => {
     const li = document.createElement("li");
     li.textContent = link;
     li.classList.add("nav-item", `${link.toLowerCase()}-link`);
     navLinks.appendChild(li);
+    navLinksCopy.appendChild(li.cloneNode(true));
   });
 
   return navElement;
