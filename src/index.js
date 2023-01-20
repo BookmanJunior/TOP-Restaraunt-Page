702bcf20f4404f6941d327bdcac28f4eef03ac20
Azam <gulovazam@gmail.com>
2023-01-20 18:45:45 +0500

    Add svg class toggler

--- src/index.js
+++ src/index.js
@@ -40,7 +40,12 @@ function tabSwitching(e) {
 function menuToggle(e) {
   if (e.target.matches(".nav-toggler")) {
     const navCollapse = document.querySelector(".nav-collapse");
+    const navLines = [...document.getElementsByClassName("line")];
+
     navCollapse.classList.toggle("display");
+    navLines.forEach((line) => {
+      line.classList.toggle("open");
+    });
   }
 }
 
