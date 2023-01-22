223235909e67bfef685a10c3661b8da56cd6d4db
Azam <gulovazam@gmail.com>
2023-01-22 22:22:52 +0500

    Add initial page module

---
+++ src/modules/initialPage.js
@@ -0,0 +1,12 @@
+import { navbar } from "./header";
+import home from "./home";
+
+export default function initialPage() {
+  document.getElementById("content").append(navbar(), home());
+  const homeLinks = document.querySelectorAll(".home-link");
+  homeLinks.forEach((link) => {
+    link.classList.add("active");
+  });
+}
+
+initialPage();
